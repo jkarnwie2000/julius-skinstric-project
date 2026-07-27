8c94bda04bece12283788e4eb3b28a432fe1f48e
jkarnwie2000 <jkarnwie2000@yahoo.com>
2026-07-26 23:55:21 -0400

    Refine photo page layout

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -150,7 +150,7 @@ if (
 <div>
 
 <div className="relative w-12 h-12 flex items-center justify-center border border-[#1A1B1C] rotate-45 scale-[1] sm:hidden">
-<span className="rotate-[-45deg] text-xs font-semibold sm:hidden">BACK</span>
+<span className="rotate-[-45deg] text-xs -semibold sm:hidden">BACK</span>
 </div>
 <div className="group hidden sm:flex flex-row relative justify-center items-center">
 <div className="w-12 h-12 hidden sm:flex justify-center border border-[#1A1B1C] rotate-45 scale-[0.85] group-hover:scale-[0.92] ease duration-300"></div>
